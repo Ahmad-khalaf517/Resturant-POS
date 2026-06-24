93f6ee1f01faea9b7423fe6ff34604ced6bc8d09
Ahmad Khalaf <ahmadkhalaf517@gmail.com>
2026-06-24 19:40:28 +0300

    Compelete setup database using prisma and supabase for db hosting

--- index.ts
+++ index.ts
@@ -1,20 +1,23 @@
-
-import { env } from "prisma/config";
-console.log( env("DATABASE_URL") );
-
-import { PrismaClient } from "./generated/prisma/index";
+import "dotenv/config";
+import { PrismaClient } from "./generated/prisma/client.ts";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
-
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 export const prisma = new PrismaClient({ adapter });
 
 async function main() {
-  // Example usage of the Prisma client
-  const users = await prisma.user.findMany();
-  console.log(users);
+  const val = await prisma.product.findMany({
+    take: 10,
+  });
+  console.log(val);
 }
 
-main();
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+});
